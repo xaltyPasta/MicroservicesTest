c8638576ece810ac59556bc0286818089d2cd937
xaltyPasta <rajkumarravi9470@gmail.com>
2024-12-12 10:42:20 +0530

    Stress Tests

--- stress.services.js
+++ stress.services.js
@@ -6,10 +6,15 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 
-app.get('/stress-test', (req, res) => {
+app.get('/', (req, res) => {
 
-    for (let i = 0; i < 100000000; i++) {
+    for (let i = 0; i < 10000000000; i++) {
 
     }
     res.send('Hello World')
+})
+
+
+app.listen(3002,()=>{
+    console.log('Server is running on http://localhost:3002');
 })
